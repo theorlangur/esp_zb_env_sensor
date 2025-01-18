feb192ea51013da7f98ae611f58fcd95dc521d03
theorlangur <theorlangur@gmail.com>
2025-01-18 11:28:10 +0100

    shaping SCD40 driver
    a more generic description of commands / commands + data / received data
    + first public methods

--- main/sensors/scd40.cpp
+++ main/sensors/scd40.cpp
@@ -8,6 +8,59 @@ SCD40::ExpectedValue<SCD40> SCD40::Open(i2c::I2CBusMaster &bus)
     return SCD40(std::move(*r));
 }
 
+uint8_t SCD40::calc_crc(const word_t &w)
+{
+    constexpr uint8_t CRC8_POLYNOMIAL = 0x31;
+    constexpr uint8_t CRC8_INIT = 0xff;
+
+    const uint8_t *data = &w.msb;
+    uint8_t crc = CRC8_INIT;
+    for(uint16_t current_byte = 0; current_byte < (sizeof(word_t) - 1); ++current_byte)
+    {
+        crc ^= data[current_byte];
+        for(uint8_t crc_bit = 8; crc_bit > 0;--crc_bit)
+        {
+            if (crc & 0x80)
+                crc = (crc << 1) ^ CRC8_POLYNOMIAL;
+            else
+                crc <<= 1;
+        }
+    }
+    return crc;
+}
+
+bool SCD40::check_crc(word_t &data)
+{
+    return calc_crc(data) == data.crc;
+}
+
 SCD40::SCD40(i2c::I2CDevice &&d): m_Device(std::move(d))
 {
+}
+
+SCD40::ExpectedValue<SCD40::measurement_results_t> SCD40::read_measurements()
+{
+    if (auto res = read_measurement{m_Device}.Recv(); !res)
+        return std::unexpected(Err{res.error(), ErrorCode::ReadMeasurements});
+    else
+    {
+        auto &r = *res;
+        return measurement_results_t{.co2 = r.co2, .temp = r.get_temp(), .rh = r.get_humidity()};
+    }
+}
+
+SCD40::ExpectedValue<SCD40::SensorType> SCD40::get_sensor_type()
+{
+    if (auto res = get_sensor_variant{m_Device}.Recv(); !res)
+        return std::unexpected(Err{res.error(), ErrorCode::GetSensorType});
+    else
+        return SCD40::SensorType{res->type};
+}
+
+SCD40::ExpectedValue<SCD40::serial_t> SCD40::get_serial_id()
+{
+    if (auto res = get_serial_number{m_Device}.Recv(); !res)
+        return std::unexpected(Err{res.error(), ErrorCode::GetSerialId});
+    else
+        return *res;
 }
--- main/sensors/scd40.hpp
+++ main/sensors/scd40.hpp
@@ -3,6 +3,9 @@
 
 #include "ph_i2c.hpp"
 
+template<class C>
+concept word_conformant_c =  requires { requires (sizeof(C) % sizeof(uint16_t) == 0); };
+
 class SCD40
 {
 public:
@@ -10,8 +13,17 @@ public:
     
     enum class ErrorCode: uint8_t
     {
-        Ok,
-        Open
+        Ok
+        , Open
+        , ReadMeasurements
+        , GetSensorType
+        , GetSerialId
+    };
+
+    enum class SensorType: uint16_t
+    {
+        SCD40 = 0,
+        SCD41 = 1
     };
 
     using Ref = std::reference_wrapper<SCD40>;
@@ -25,19 +37,38 @@ public:
     template<class V>
     using ExpectedValue = std::expected<V, Err>;
 
+    struct measurement_results_t
+    {
+        uint16_t co2;
+        float temp;
+        float rh;
+    };
+
+    struct serial_t
+    {
+        uint16_t w[3];
+    };
+
     static ExpectedValue<SCD40> Open(i2c::I2CBusMaster &bus);
 
     SCD40(SCD40 &&d) = default;
     SCD40(const SCD40 &d) = delete;
+
+    ExpectedValue<measurement_results_t> read_measurements();
+    ExpectedValue<SensorType> get_sensor_type();
+    ExpectedValue<serial_t> get_serial_id();
 private:
     SCD40(i2c::I2CDevice &&d);
     struct word_t
     {
+        word_t() = default;
+        word_t(uint16_t d);
+
         uint8_t msb;
         uint8_t lsb;
         uint8_t crc;
     };
-    static uint8_t calc_crc(uint16_t data);
+    static uint8_t calc_crc(const word_t &w);
     static bool check_crc(word_t &data);
 
     enum class Regs: uint16_t
@@ -80,6 +111,52 @@ private:
         get_automatic_self_calibration_standard_period = 0x234b,
     };
 
+    template<size_t N>
+    struct cmd_data_t
+    {
+        cmd_data_t(Regs cmd): cmd_msb(uint16_t(cmd) >> 8), cmd_lsb(uint16_t(cmd) & 0x0ff){}
+        template<class V> requires word_conformant_c<V> && (sizeof(V) == sizeof(uint16_t) * N)
+        cmd_data_t(Regs cmd, V const& v): cmd_data_t(cmd)
+        {
+            const uint16_t *pSrc = reinterpret_cast<const uint16_t*>(&v);
+            for(word_t &w : data)
+            {
+                w.msb = *pSrc >> 8;
+                w.lsb = *pSrc & 0x0ff;
+                w.crc = calc_crc(w);
+            }
+        }
+
+        operator uint8_t*() { return &cmd_msb; }
+        operator const uint8_t*() const { return &cmd_msb; }
+
+        uint8_t cmd_msb;
+        uint8_t cmd_lsb;
+        [[no_unique_address]] word_t data[N];
+    };
+    template<class V>
+    cmd_data_t(Regs cmd, V const& v) -> cmd_data_t<sizeof(V) / sizeof(uint16_t)>;
+
+    template<class V>
+    struct recv_data_t
+    {
+        operator uint8_t *() { return (uint8_t*)this; }
+
+        bool read(V &dst)
+        {
+            uint16_t *pDst = (uint16_t *)&dst;
+            for(word_t &w : data)
+            {
+                if (!check_crc(w))
+                    return false;
+                *pDst = w.msb << 8 | w.lsb;
+                ++pDst;
+            }
+            return true;
+        }
+        word_t data[sizeof(V) / sizeof(uint16_t)];
+    };
+
     struct RegisterBase
     {
         i2c::I2CDevice &d;
@@ -90,85 +167,51 @@ private:
     {
         i2c::I2CDevice::ExpectedResult Send()
         {
-            uint8_t data[] = {uint8_t(uint16_t(r) >> 8), uint8_t(uint16_t(r) & 0x00ff)};
+            cmd_data_t<0> data{r};
             return d.Send(data, sizeof(data), i2c::helpers::kTimeout);
         }
     };
 
-    template<Regs r, class Val> requires (sizeof(Val) == sizeof(uint16_t))
+    template<Regs r, class Val> requires word_conformant_c<Val>
     struct RegWrite: RegisterBase
     {
         i2c::I2CDevice::ExpectedResult Send(Val const& v)
         {
-            uint8_t data[] = 
-            {     uint8_t(uint16_t(r) >> 8)                        //reg MSB
-                , uint8_t(uint16_t(r) & 0x00ff)                    //reg LSB
-                , uint8_t(reinterpret_cast<uint16_t&>(v) >> 8)     //data MSB
-                , uint8_t(reinterpret_cast<uint16_t&>(v) & 0x00ff) //data LSB
-                , calc_crc(reinterpret_cast<uint16_t&>(v))         //data CRC
-            };
+            cmd_data_t data{r, v};
             return d.Send(data, sizeof(data), i2c::helpers::kTimeout);
         }
     };
 
-    template<Regs r, class Val> requires (sizeof(Val) % sizeof(uint16_t) == 0)
+    template<Regs r, class Val> requires word_conformant_c<Val>
     struct RegRead: RegisterBase
     {
         std::expected<Val, ::Err> Recv()
         {
-            uint8_t reg_data[] = 
-            {     uint8_t(uint16_t(r) >> 8)                        //reg MSB
-                , uint8_t(uint16_t(r) & 0x00ff)                    //reg LSB
-            };
-            word_t data[sizeof(Val) / sizeof(uint16_t)];
+            cmd_data_t<0> reg_data{r};
+            recv_data_t<Val> data;
             if (auto ret = d.SendRecv(reg_data, sizeof(reg_data), data, sizeof(data), i2c::helpers::kTimeout); !ret)
                 return std::unexpected(ret.error());
 
             Val res;
-            uint16_t *pDst = (uint16_t *)&res;
-            for(word_t &w : data)
-            {
-                if (!check_crc(w))
-                    return std::unexpected(::Err{"RegRead", ESP_ERR_INVALID_CRC});
-                *pDst = w.msb << 8 | w.lsb;
-                ++pDst;
-            }
+            if (!data.read(res))
+                return std::unexpected(::Err{"RegRead", ESP_ERR_INVALID_CRC});
             return res;
         }
     };
 
-    template<Regs r, class SendType, class RecvType> requires ((sizeof(SendType) % sizeof(uint16_t) == 0) && (sizeof(RecvType) % sizeof(uint16_t) == 0))
+    template<Regs r, class SendType, class RecvType> requires word_conformant_c<SendType> && word_conformant_c<RecvType>
     struct RegReadWrite: RegisterBase
     {
         std::expected<RecvType, ::Err> Transmit(SendType s)
         {
-            uint8_t reg_data[2 + 3 * (sizeof(SendType) / sizeof(uint16_t))] = 
-            {     uint8_t(uint16_t(r) >> 8)                        //reg MSB
-                , uint8_t(uint16_t(r) & 0x00ff)                    //reg LSB
-                , uint8_t()
-                , uint8_t(reinterpret_cast<uint16_t&>(s))
-                , calc_crc(reinterpret_cast<uint16_t&>(s))
-            };
-            uint8_t *pRegDst = reg_data + 2;
-            for(uint16_t *pSrc = (uint16_t *)&s; pSrc != (uint16_t *)(&s + 1); ++pSrc, pRegDst += 3)
-            {
-                pRegDst[0] = *pSrc >> 8;
-                pRegDst[1] = *pSrc;
-                pRegDst[2] = calc_crc(*pSrc);
-            }
-            word_t data[sizeof(RecvType) / sizeof(uint16_t)];
+            cmd_data_t reg_data{r, s};
+            recv_data_t<RecvType> data;
             if (auto ret = d.SendRecv(reg_data, sizeof(reg_data), data, sizeof(data), i2c::helpers::kTimeout); !ret)
                 return std::unexpected(ret.error());
 
             RecvType res;
-            uint16_t *pDst = (uint16_t *)&res;
-            for(word_t &w : data)
-            {
-                if (!check_crc(w))
-                    return std::unexpected(::Err{"RegReadWrite", ESP_ERR_INVALID_CRC});
-                *pDst = w.msb << 8 | w.lsb;
-                ++pDst;
-            }
+            if (!data.read(res))
+                return std::unexpected(::Err{"RegReadWrite", ESP_ERR_INVALID_CRC});
             return res;
         }
     };
@@ -234,20 +277,11 @@ private:
     };
     using get_data_ready_status = RegWrite<Regs::get_data_ready_status, data_ready_t>;
     using save_settings = RegCmd<Regs::persist_settings>;
-    struct serial_t
-    {
-        uint16_t w[3];
-    };
     using get_serial_number = RegRead<Regs::get_serial_number, serial_t>;
     using perform_self_test = RegRead<Regs::perform_self_test, uint16_t>;
     using perform_factory_reset = RegCmd<Regs::perform_factory_reset>;
     using reinit = RegCmd<Regs::reinit>;
 
-    enum class SensorType: uint16_t
-    {
-        SCD40 = 0,
-        SCD41 = 1
-    };
     struct sensor_variant_t
     {
         uint16_t unused: 12;
