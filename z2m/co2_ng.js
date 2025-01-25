c444e7990c89c9fd29a75a889d7e0bb11945390e
theorlangur <theorlangur@gmail.com>
2025-01-25 19:47:18 +0100

    modified z2m converter to manually issue a voltage reading command

--- z2m/co2_ng.js
+++ z2m/co2_ng.js
@@ -1,11 +1,29 @@
 const {battery, co2, temperature, humidity} = require('zigbee-herdsman-converters/lib/modernExtend');
 
+const power_cfg_ext = {
+    battery_read_voltage: () => {
+        const res = battery({voltage:true});
+        res.fromZigbee.push({
+                cluster: 'genPowerCfg',
+                type: ['attributeReport', 'readResponse'],
+                convert: (model, msg, publish, options, meta) => {
+                    if (msg.data.batteryPercentageRemaining !== undefined && msg.data['batteryPercentageRemaining'] < 255) {
+                        const endpoint = meta.device.getEndpoint(1);
+                        await endpoint.read('genPowerCfg', ['batteryVoltage']);
+                    }
+                    return {}
+                }
+            })
+        return res;
+    }
+}
+
 const definition = {
     zigbeeModel: ['Co2-NG'],
     model: 'Co2-NG',
     vendor: 'Orlangur',
     description: 'Automatically generated definition',
-    extend: [battery({voltage:true}), co2(), temperature(), humidity()],
+    extend: [power_cfg_ext.battery_read_voltage(), co2(), temperature(), humidity()],
     meta: {},
 };
 
